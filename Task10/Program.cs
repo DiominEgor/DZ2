1e231186ccedcd4609745ee6ee89ce02c5c6c700
Egor Diomin <diomin.egor@gmail.com>
2023-08-11 03:11:24 +0300

    Completed task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int DelSecDigit(int num)
+{
+    int delLastDigit = num / 10;
+    int result = delLastDigit % 10;
+    return (result);
+}
+
+Console.Write("Введите число от 100 до 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 999 || number < 100)
+{
+    Console.Write("Неверное число!");
+}
+else
+{
+    int result = DelSecDigit(number);
+    Console.WriteLine($"{result}");
+}
