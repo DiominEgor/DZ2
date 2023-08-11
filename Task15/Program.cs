8e26b4e16f8cea67a5a47e1feda029b99300cd80
Egor Diomin <diomin.egor@gmail.com>
2023-08-11 03:44:50 +0300

    Completed task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+string CheckWeekend(int num)
+{
+    if (num > 7 || num < 0)
+    {
+        return ("Такого номера дня недели не существует!");
+    }
+    else if (num < 6)
+    {
+        return ("Нет.");
+    }
+    else
+    {
+        return ("Да.");
+    }
+}
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = CheckWeekend(number);
+Console.Write(result);
